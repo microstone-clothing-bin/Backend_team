a30e4616c6f20c8d335a4e524bee4193fdceef1c
Junwon0653 <dnjsvkdlxld@naver.com>
2025-07-08 15:39:46 +0900

    Initial commit

---
+++ src/main/java/com/example/cloth_area/ClothAreaApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloth_area;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothAreaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClothAreaApplication.class, args);
+    }
+
+}
